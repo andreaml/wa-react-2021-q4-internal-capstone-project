1eec2004cb64b8189d8955bdea64f0b9b786f161
Andrea <andrea.munoz@wizeline.com>
2022-04-23 18:19:25 -0500

    Removed comments

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Header from '.';
 
-// Question: how to include screen in the eslint scope?
 test('header logo image loads', () => {
   render(<Header />);
 
